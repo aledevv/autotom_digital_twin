7d1b9b087c53511ebbcc78ffd34e9943a9c683b0
aledevv <alessandro.devidi@studenti.unitn.it>
2026-07-08 09:59:48 +0100

    BACKUP 1.5: changed paths adding USD test model files + testing everything is working

--- src/plant_model/main.py
+++ src/plant_model/main.py
@@ -7,7 +7,7 @@ day = 160
 plant_id = 1
 
 for day in range(1, 161):
-    snapshot = load_snapshot(f"data/dynamic_output/graphs/graph_day_{day}.csv", day=day, plant_id=plant_id)
+    snapshot = load_snapshot(f"data/simulation_output/dynamic_output/graphs/graph_day_{day}.csv", day=day, plant_id=plant_id)
     visualize_snapshot(snapshot, f"./output/day_{day}/plant_day{day}.html")
     export_graph_json(snapshot, f"./output/day_{day}/plant_day{day}.json") # just for debug
     export_plant_usd(snapshot, f"./output/day_{day}/plant_day{day}.usda")
