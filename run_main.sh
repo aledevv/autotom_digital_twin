531d7cdf92b98ac84c0e58c714bcd0776eedaf3c
aledevv <alessandro.devidi@studenti.unitn.it>
2026-07-27 16:30:39 +0100

    removed unused files

--- run_main.sh
+++ run_main.sh
@@ -6,4 +6,4 @@ ISAACSIM_DIR="$HOME/isaacsim"
 MAIN_V1="$SCRIPT_DIR/src/plant_model/main.py"
 
 echo "=== Loading Stem V1 in Isaac Sim ==="
-"$ISAACSIM_DIR/python.sh" "$MAIN_V1" --day 20 --plant 1
+"$ISAACSIM_DIR/python.sh" "$MAIN_V1" --day 1 --plant 1
